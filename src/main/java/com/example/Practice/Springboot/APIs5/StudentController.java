5e7568c64b50bfbf1abb6d2c4d3ff6e8a1ab868a
Wala AL mawali <wala.salam.9888@gmail.com>
2026-06-07 10:26:09 +0400

    Create StudentController class + create updateStudent function

---
+++ src/main/java/com/example/Practice/Springboot/APIs5/StudentController.java
@@ -0,0 +1,19 @@
+package com.example.Practice.Springboot.APIs5;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    Student student = new Student(100,"Ahmed","A");
+
+    @PutMapping("/updateStudent")
+    public String updateStudent(@RequestParam String name){
+        String oldName = student.getStudentName();
+        student.setStudentName(name);
+
+        return "Student name updated successfully from : " + oldName + " to " + student.getStudentName();
+    }
+}
